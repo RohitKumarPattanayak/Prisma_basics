e267f8c4b90d0bab20e2c0c676595d4cce40c63d
RohitKumarPattanayak <rohitkumarpattanayak@gmail.com>
2023-01-28 23:03:36 +0530

    added fetch API

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = 5000;
+app.get("/", async (req: Request, res: Response) => {
+  return res.status(200).send({
+    message: `server running at port: ${process.env.PORT} `,
+  });
+});
 
 app.post("/create", async (req: Request, res: Response) => {
   let { data } = req.body;
@@ -20,10 +24,16 @@ app.post("/create", async (req: Request, res: Response) => {
     message: "successfully created users",
   });
 });
+app.get("/getUsers", async (req: Request, res: Response) => {
+  const data = await prisma.users.findMany({});
+  return res.status(200).send({
+    message: data,
+  });
+});
 
 try {
-  app.listen(PORT, (): void => {
-    console.log(`Connected successfully on port ${PORT}`);
+  app.listen(process.env.PORT, (): void => {
+    console.log(`Connected successfully on port ${process.env.PORT}`);
   });
 } catch (error: any) {
   console.error(`Error occured: ${error.message}`);
