35d2b65ce7a5754e8f345b0f467005f871623d96
RohitKumarPattanayak <rohitkumarpattanayak@gmail.com>
2023-01-30 17:59:48 +0530

    made all API required for tenzies game

--- src/index.ts
+++ src/index.ts
@@ -18,32 +18,66 @@ let router = express.Router();
 
 app.post("/createUser", async (req: Request, res: Response) => {
   let { data } = req.body;
-  console.log(JSON.stringify(data));
-  await prisma.users.create({
-    data,
-    include: {
-      profile: true,
-    },
-  });
-  return res.status(200).send({
-    message: "successfully created users",
-  });
+  try {
+    const result = await prisma.users.create({
+      data: data,
+    });
+    return res.status(200).send({
+      message: `successfully created users ${JSON.stringify(result)}`,
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
-app.get("/getUsers", async (req: Request, res: Response) => {
-  const data = await prisma.users.findMany({
-    select: {
-      name: true,
-      profile: {
-        select: {
-          bio: true,
-        },
+
+app.put("/putUserDetails/:id/:score", async (req: Request, res: Response) => {
+  const { id, score } = req.params;
+  try {
+    await prisma.users.update({
+      where: { id: Number(id) },
+      data: { total_score: Number(score) },
+    });
+    return res.status(200).json({
+      message: `successfully updated user details`,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/getUserDetails", async (req: Request, res: Response) => {
+  try {
+    const result = await prisma.users.findMany({
+      select: {
+        id: true,
+        name: true,
+        total_score: true,
+        updatedAt: true,
       },
-    },
-  });
-  return res.status(200).send({
-    message: data,
-  });
+    });
+    return res.status(200).json({
+      message: `successfully fetched user details`,
+      data: result,
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
+// app.get("/getUsers", async (req: Request, res: Response) => {
+//   const data = await prisma.users.findMany({
+//     select: {
+//       name: true,
+//       profile: {
+//         select: {
+//           bio: true,
+//         },
+//       },
+//     },
+//   });
+//   return res.status(200).send({
+//     message: data,
+//   });
+// });
 
 try {
   app.listen(process.env.PORT, (): void => {
